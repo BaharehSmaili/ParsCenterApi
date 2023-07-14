96f9cdad8f4e096eca46d2f47ebfc4135eaf32a5
bahareh smaili <bahareh_smaili_1990@yahoo.com>
2023-07-15 02:32:57 +0330

    add Automatic Validation

--- ParsCenterApi/Controllers/UserController.cs
+++ ParsCenterApi/Controllers/UserController.cs
@@ -4,6 +4,7 @@ using Entities.Models.User;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using ParsCenterApi.Models;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
@@ -14,6 +15,7 @@ namespace ParsCenterApi.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ApiResultFilter]
     public class UserController : ControllerBase
     {
         private readonly IUserRepository userRepository;
@@ -24,7 +26,6 @@ namespace ParsCenterApi.Controllers
         }
 
         [HttpGet]
-        [ApiResultFilter]
         public async Task<List<User>> Get(CancellationToken cancellationToken)
         {
             var users = await userRepository.TableNoTracking.ToListAsync(cancellationToken);
@@ -42,39 +43,52 @@ namespace ParsCenterApi.Controllers
         }
 
         [HttpPost]
-        public async Task<ApiResult<User>> Create(User user, CancellationToken cancellationToken)
+        public async Task<ApiResult<User>> Create(UserDto userDto, CancellationToken cancellationToken)
         {
-            await userRepository.AddAsync(user, cancellationToken);
-            return Ok(user);
+
+            //var exists = await userRepository.TableNoTracking.AnyAsync(p => p.UserName == userDto.UserName);
+            //if (exists)
+            //    return BadRequest("نام کاربری تکراری است");
+
+            var user = new User
+            {
+                Name = userDto.Name,
+                Family = userDto.Family,
+                NationalCode = userDto.NationalCode,
+                Mobile = userDto.Mobile,
+                Email = userDto.Email
+            };
+            await userRepository.AddAsync(user, userDto.Password, cancellationToken);
+            return user;
         }
 
-        [HttpPut]
-        public async Task<ApiResult> Update(Guid id, User user, CancellationToken cancellationToken)
-        {
-            var updateUser = await userRepository.GetByIdAsync(cancellationToken, id);
+    [HttpPut]
+    public async Task<ApiResult> Update(Guid id, User user, CancellationToken cancellationToken)
+    {
+        var updateUser = await userRepository.GetByIdAsync(cancellationToken, id);
 
-            updateUser.Mobile = user.Mobile;
-            updateUser.Password = user.Password;
-            updateUser.Name = user.Name;
-            updateUser.Family = user.Family;
-            updateUser.NationalCode = user.NationalCode;
-            updateUser.Email = user.Email;
-            updateUser.Country = user.Country;
-            updateUser.State = user.State;
-            updateUser.City = user.City;
+        updateUser.Mobile = user.Mobile;
+        updateUser.Password = user.Password;
+        updateUser.Name = user.Name;
+        updateUser.Family = user.Family;
+        updateUser.NationalCode = user.NationalCode;
+        updateUser.Email = user.Email;
+        updateUser.Country = user.Country;
+        updateUser.State = user.State;
+        updateUser.City = user.City;
 
-            await userRepository.UpdateAsync(updateUser, cancellationToken);
+        await userRepository.UpdateAsync(updateUser, cancellationToken);
 
-            return Ok();
-        }
-
-        [HttpDelete]
-        public async Task<ApiResult> Delete(Guid id, CancellationToken cancellationToken)
-        {
-            var user = await userRepository.GetByIdAsync(cancellationToken, id);
-            await userRepository.DeleteAsync(user, cancellationToken);
-
-            return Ok();
-        }
+        return Ok();
     }
+
+    [HttpDelete]
+    public async Task<ApiResult> Delete(Guid id, CancellationToken cancellationToken)
+    {
+        var user = await userRepository.GetByIdAsync(cancellationToken, id);
+        await userRepository.DeleteAsync(user, cancellationToken);
+
+        return Ok();
+    }
+}
 }
---
+++ ParsCenterApi/Models/UserDto.cs
@@ -0,0 +1,54 @@
+﻿using Entities;
+using Entities.Models.BasicInformation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParsCenterApi.Models
+{
+    public class UserDto : IValidatableObject
+    {
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Family { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string NationalCode { get; set; }
+
+        [Required]
+        [StringLength(11)]
+        public string Mobile { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Password { get; set; }
+
+        public int CountryId { get; set; }
+        public int StateId { get; set; }
+        public int CityId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (NationalCode.Equals("0000000000", StringComparison.OrdinalIgnoreCase))
+                yield return new ValidationResult("نام کاربری نمی تواند 0000000000 باشد", new[] { nameof(NationalCode) });
+            if (NationalCode.Equals("1111111111", StringComparison.OrdinalIgnoreCase))
+                yield return new ValidationResult("نام کاربری نمی تواند 1111111111 باشد", new[] { nameof(NationalCode) });
+            if (Password.Equals("123456"))
+                yield return new ValidationResult("رمز عبور نمی تواند 123456 باشد", new[] { nameof(Password) });
+            if (!Email.Contains('@'))
+                yield return new ValidationResult("ایمیل حتما باید @ داشته باشد", new[] { nameof(Email) });
+        }
+    }
+}
